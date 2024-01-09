a65cc765d92557aeb15738c0b82f4511859704a6
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-09 15:49:31 +0100

    fix: mostrare a schermo i dati dell'array tramite ciclo for in php

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
+
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +48,21 @@
     <title>PHP Hotel</title>
 </head>
 <body>
+    <h1>ciao</h1>
     
+    <?php 
+         for($i = 0; $i < count($hotels); $i++){ 
+           echo $name =  $hotels[$i]['name'];
+           echo '<br>';
+           echo $desc = $hotels[$i]['description'];
+           echo '<br>';
+           echo $park = $hotels[$i]['parking'];
+           echo '<br>';
+           echo $vote =  $hotels[$i]['vote'];
+           echo '<br>';
+           echo $distance = $hotels[$i]['distance_to_center'];
+           echo '<br>';
+        }
+    ?>
 </body>
 </html>
